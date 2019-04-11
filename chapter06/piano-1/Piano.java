a209a5ffab931b2915c21003f1cc862a7520825f
Selena Yang <syang@lcs.on.ca>
2019-04-11 10:07:51 -0400

    showing a list of names from the array on screen

--- chapter06/piano-1/Piano.java
+++ chapter06/piano-1/Piano.java
@@ -13,33 +13,47 @@ public class Piano extends World
      * (we can use these variables anywhere in the class)
      */
     int frames;
-    
+
     // Create the array(list of names)
     String[] peopleInClass= {"Julio", "Liam", "Dima", "Nick", "Simon", 
-        "Osman", "Eric", "Phillip", "Joven",  "Joe", "Gordon", "Jieun", 
-        "Selena", "Jason", "Sam", "Jeff"};
-    
+            "Osman", "Eric", "Phillip", "Joven",  "Joe", "Gordon", "Jieun", 
+            "Selena", "Jason", "Sam", "Jeff"};
+
     /**
      * Make the piano.
      */
     public Piano() 
     {
         super(800, 340, 1);
-        
+
         //Initialize instance variables
         frames = 0;
     }
-    
+
     /**
      * act
      * (gets called repeatedly to create animation)
      */
     public void act()
     {
-        //Track frames
-        frames += 1;
-        
+        //Say hello to everyone in the class!
+        if (frames % 60 == 0)
+        {
+            //Only show a message when we are in the bounds of the array
+            showText("Array index is:" + frames / 60, 400, 250);
+
+            // Only say hello when we are in the bounds of the array
+            if (frames / 60 < 16)
+            {
+                //Track frames
+                showText("Hello" + peopleInClass[frames / 60], 400, 170);
+            }
+
+        }
         // show the frames
+        frames += 1;
+
+        // Show the frames
         showText("" + frames, 100, 100);
     }
 }

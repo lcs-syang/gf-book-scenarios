1ae68dda90636d6f0e58d877cedc3c4fd78035df
Selena Yang <syang@lcs.on.ca>
2019-04-13 10:34:58 -0400

    fixed the press of the key

--- chapter06/piano-1/Key.java
+++ chapter06/piano-1/Key.java
@@ -3,10 +3,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 public class Key extends Actor
 {
     /**
+     * Instance variables
+     * These can be used anywhere in the class below
+     */
+    //Will keep track of whether a key was just pressed
+    private boolean keyAlreadyDown;
+    
+    
+    /**
      * Create a new key.
      */
     public Key()
     {
+        // No key has been pressed
+        keyAlreadyDown = false;
+        
     }
 
     /**
@@ -15,16 +26,20 @@ public class Key extends Actor
     public void act()
     {
         // Amimate the piano key being pressed
-        if(Greenfoot.isKeyDown("g"))
+        if(Greenfoot.isKeyDown("g") && keyAlreadyDown == false)
         {
             setImage("white-key-down.png");
             play();
-          
+            keyAlreadyDown = true;
         }
-        else
+        
+        //Stop showing the key being pressed
+        //Condition 1 - the key was down the last time act() fired
+        //Condition 2- the "g" has been released no longer being pressed
+        if ( keyAlreadyDown == true && Greenfoot.isKeyDown("g") == false)
         {
             setImage("white-key.png");
-
+            keyAlreadyDown = false;
         }
         
     }
